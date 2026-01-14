2c59e186471eea6c824d829b9527337e70de7330
karthiknishanth <karthik.nishanth06@gmail.com>
2026-01-14 22:53:21 +0530

    refactor: reintroduce GlobalError component and update Sentry configuration for improved error handling

--- next.config.ts
+++ next.config.ts
@@ -25,49 +25,18 @@ const nextConfig: NextConfig = {
 };
 
 export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
   org: "karthik-lq7",
-
   project: "cohorts",
-
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // Disable all automatic instrumentation to fix Next.js 16 compatibility
+  disableAutoInstrumentation: true,
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: "/monitoring",
-
-  // Disable automatic instrumentation to avoid global-error build issues
-  autoInstrumentAppDirectory: false,
-  autoInstrumentServerFunctions: false,
-  autoInstrumentMiddleware: false,
-
-  // Exclude global-error from source map upload
+  // Disable source maps upload to avoid global-error.rsc issues
   sourcemaps: {
-    ignore: ["**/global-error*"],
+    disable: true,
   },
 
-  webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-
-    // Tree-shaking options for reducing bundle size
-    treeshake: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      removeDebugLogging: true,
-    },
-  },
+  // Disable Vercel Cron monitoring
+  automaticVercelMonitors: false,
 });
